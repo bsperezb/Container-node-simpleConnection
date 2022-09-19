21edb95286c34a09586f5151198fba1615d88b78
Brayan Pérez Buitrago <83848297+bsperezb@users.noreply.github.com>
2022-09-18 21:43:11 -0500

    Test VSonline

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 mongoose.connect('mongodb://nico:password@monguito:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
-  console.log('listando... chanchitos 123...')
+  console.log('listando... chanchitos ...')
   const animales = await Animal.find();
   return res.send(animales)
 })
